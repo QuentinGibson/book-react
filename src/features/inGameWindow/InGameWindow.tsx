cbba6cd3f4e4061bad501f2116a3767d3a519ad6
QuentinGibson <qgibson3@students.kennesaw.edu>
2022-02-26 22:05:45 -0500

    update manifest to show in all games

--- src/features/inGameWindow/InGameWindow.tsx
+++ src/features/inGameWindow/InGameWindow.tsx
@@ -1,42 +1,42 @@
-import { useEffect } from 'react'
-import { Title } from 'components/Title/Title'
-import { Feed } from 'components/Feed'
-import { RootReducer } from 'app/rootReducer'
-import { useSelector } from 'react-redux'
-import './InGame.css'
+import { useEffect } from "react";
+import { Title } from "components/Title/Title";
+import { Feed } from "components/Feed";
+import { RootReducer } from "app/rootReducer";
+import { useSelector } from "react-redux";
+import "./InGame.css";
 
 const InGameWindow = () => {
-  const { event, info } = useSelector((state: RootReducer) => state.background)
+  const { event, info } = useSelector((state: RootReducer) => state.background);
 
   useEffect(() => {
     console.info(
-      '[🐺 overwolf-modern-react-boilerplate][🧰 InGameWindow][🔧 useEffect - event]',
-      JSON.stringify(event, null, 2),
-    )
+      "[🐺 overwolf-modern-react-boilerplate][🧰 InGameWindow][🔧 useEffect - event]",
+      JSON.stringify(event, null, 2)
+    );
     // or use https://github.com/AlbericoD/overwolf-modern-react-boilerplate#-remote-redux-debug
-  }, [event])
+  }, [event]);
 
   useEffect(() => {
     console.info(
-      '[🐺 overwolf-modern-react-boilerplate][🧰 InGameWindow][🔧 useEffect -info]',
-      JSON.stringify(info, null, 2),
-    )
+      "[🐺 overwolf-modern-react-boilerplate][🧰 InGameWindow][🔧 useEffect -info]",
+      JSON.stringify(info, null, 2)
+    );
     // or use https://github.com/AlbericoD/overwolf-modern-react-boilerplate#-remote-redux-debug
-  }, [info])
+  }, [info]);
 
   return (
     <div className="in-game-container">
-      <Title color="white">In Game Window</Title>
+      <Title color="white">In Game Window for all games</Title>
       <Feed
         title="Events"
-        data={event.length ? event[0] : { content: 'No events yet' }}
+        data={event.length ? event[0] : { content: "No events yet" }}
       />
       <Feed
         title="Infos"
-        data={Object.keys(info).length ? info : { content: 'No infos yet' }}
+        data={Object.keys(info).length ? info : { content: "No infos yet" }}
       />
     </div>
-  )
-}
+  );
+};
 
-export default InGameWindow
+export default InGameWindow;
